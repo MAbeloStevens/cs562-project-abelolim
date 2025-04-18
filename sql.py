0ca6b86a6023920b9b60b8fea0ef9daf04925516
MAbeloStevens <124626324+MAbeloStevens@users.noreply.github.com>
2025-04-18 10:33:26 -0400

    Update sql.py

--- sql.py
+++ sql.py
@@ -18,7 +18,24 @@ def query():
     conn = psycopg2.connect("dbname="+dbname+" user="+user+" password="+password,
                             cursor_factory=psycopg2.extras.DictCursor)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM sales WHERE quant > 10")
+    cur.execute("""WITH ny as (
+    SELECT prod, avg(quant) ny_avg
+    FROM Sales
+    WHERE state = 'NY'
+    GROUP BY prod
+), nj as (
+    SELECT prod, avg(quant) nj_avg
+    FROM Sales
+    WHERE state = 'NJ'
+    GROUP BY prod
+), ct as (
+    SELECT prod, avg(quant) ct_avg
+    FROM Sales
+    WHERE state = 'CT'
+    GROUP BY prod
+)
+SELECT prod, ny_avg, nj_avg, ct_avg
+FROM ny natural join nj natural join ct WHERE ny_avg > nj_avg or ny_avg > ct_avg""")
 
     return tabulate.tabulate(cur.fetchall(),
                              headers="keys", tablefmt="psql")
